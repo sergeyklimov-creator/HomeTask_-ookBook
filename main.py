ab290206b62d44442e3e938efcd241ccdf90cedf
sergeyklimov-creator <s.klimov@bk.ru>
2020-01-16 12:12:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+def get_cook_book(file_name):
+    cook_book = {}
+    
+    with open(file_name) as f:        
+        while True:
+            key_name = f.readline().rstrip()            
+            if not key_name:
+                break
+            items_count = int(f.readline().rstrip())          
+            dish_list = []
+            for foo in range(items_count):
+                ingredient_list = f.readline().rstrip().split(' | ')
+                if ingredient_list:
+                    dish_list.append({'ingredient_name': ingredient_list[0],
+                                    'quantity': ingredient_list[1],
+                                    'measure': ingredient_list[2]
+                    })
+    
+            f.readline().rstrip() 
+            cook_book.setdefault(key_name, dish_list)
+           
+    return cook_book
+
+def print_cook_book(book):
+    for key in book:
+        print(key)        
+        [print(foo) for foo in book[key]]
+
+def get_shop_list_by_dishes(dishes, person_count):
+    ingredient_dict = {}
+    cook_book = get_cook_book('cookbook.txt')
+    for dish in dishes:
+        for ingredient_list in cook_book[dish]:            
+            if ingredient_list['ingredient_name'] not in ingredient_dict.keys():
+                ingredient_dict.setdefault(ingredient_list['ingredient_name'], 
+                                        {'measure': ingredient_list['measure'],
+                                        'quantity': int(ingredient_list['quantity']) * person_count})
+            else:
+                ingredient_dict[ingredient_list['ingredient_name']]['quantity'] += \
+                    int(ingredient_list['quantity']) * person_count
+
+    return ingredient_dict
+
+def print_shop_list_by_dishes(ingredients):
+    [print(key, ': ', ingredients[key]) for key in ingredients]
+
+def main():
+    print('Задача 1')
+    print_cook_book(get_cook_book('cookbook.txt'))
+
+    user_list = ['Запеченный картофель', 'Омлет']
+    print(f"\nЗадача 2.1\nСоставляем список покупок для {user_list}")
+    print_shop_list_by_dishes(get_shop_list_by_dishes(user_list, 2))
+
+    user_list = ['Фахитос', 'Омлет']
+    print(f"\nЗадача 2.2 c пересечением ингридиентов\nСоставляем список покупок для {user_list}")
+    print_shop_list_by_dishes(get_shop_list_by_dishes(user_list, 2))
+
+main()
